ec97a9f54361a8c4b5b88677a33fcbf5bc197a55
mooncinnamon <moon.pinnamon@gmail.com>
2018-11-19 07:25:45 +0900

    update App Route

--- 3bytes/src/containers/App.js
+++ 3bytes/src/containers/App.js
@@ -1,7 +1,28 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import store from "../stores";
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+import PrivateRoute from './PrivateRoute';
+
+import {Login, Slack, Regist} from "../components/index";
+import setAuthToken from "../libs/setAuthToken";
+import jwt_decode from "jwt-decode";
+import store from "../store";
+import {logoutUser, setCurrentUser} from "../actions/auth";
 import Provider from "react-redux/es/components/Provider";
+
+if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decoded = jwt_decode(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(decoded));
+
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        store.dispatch(logoutUser());
+    }
+}
+
 
 class App extends Component {
     render() {
@@ -9,7 +30,10 @@ class App extends Component {
             <Provider store={store}>
                 <Router>
                     <div>
-
+                        <Header/>
+                        <Route exact path={"/login"} component={Login}/>
+                        <Route exact path={"/regist"} component={Regist}/>
+                        <PrivateRoute exact path={"/slack"} component={Slack}/>
                     </div>
                 </Router>
             </Provider>
